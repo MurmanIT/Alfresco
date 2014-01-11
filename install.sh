737f5a8be1c704b05096ab61cf223f85ce94f601
Anton Drelin <work@murmanit.ru>
2014-01-12 00:29:03 +0400

    Begin work on install of Alfresco version 1.0 Anton Drelin <work@murmanit.ru>

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Script install of Alfresco on Ubuntu
+# Author Anton Drelin
+
+export ALF_HOME =/opt/alfresco
+export TOMCAT=$ALF_HOME/tomcat
+export ALF_USER=alfresco
+
+#Russian locate
+export UTF=ru_RU.utf8
+
